16469ab00476acf46da00ed977a3fed2faeda144
Daniel Herculano <98058633+herculanodev@users.noreply.github.com>
2023-10-04 01:42:34 -0300

    Subir arquivos
    Venho por meio deste dar inicio a nosso projeto

---
+++ exer1.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import sys
+
+# Questão 1:
+# Ajuste as idades que não são válidas ou estão vazias para a moda da amostra. Grave a saída no arquivo Resposta01.txt
+
+# Tenta ler o arquivo CSV
+try:
+    data = pd.read_csv('titanic.csv')
+except FileNotFoundError as e:
+    print(f"Erro: O arquivo 'titanic.csv' não foi encontrado.")
+    sys.exit()
+
+# Calcula a moda da idade
+moda_idade = data['age'].mode()[0]
+
+# Preenche os valores em branco na coluna 'age' com a moda
+data['age'].fillna(moda_idade, inplace=True)
+
+# Salva os dados ajustados no arquivo 'Resposta01.txt'
+data.to_csv('Resposta01.txt', index=False)
+
+# Confirmação de que a operação foi concluída
+print("Os valores de idade inválidos foram ajustados e o resultado foi salvo em 'Resposta01.txt'.")
---
+++ exer3.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+# Tenta ler o arquivo CSV
+try:
+    data = pd.read_csv('titanic.csv')
+except FileNotFoundError as e:
+    print(f"Erro: O arquivo 'titanic.csv' não foi encontrado.")
+    sys.exit()
+
+# Questão 3:
+# Considerando a coluna "survived", sendo 0 como não sobrevivente e 1 como sobrevivente, apresente em um gráfico de pizza a porcentagem de sobreviventes e não sobreviventes.
+
+survived = (data['survived'] == 1).sum()
+n_survived = (data['survived'] == 0).sum()
+
+# Criação do gráfico de pizza
+plt.figure(figsize=(6, 6))
+labels = ['Sobreviventes', 'Não Sobreviventes']
+sizes = [survived, n_survived]
+colors = ['green', 'red']
+explode = (0.1, 0)
+
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+
+plt.title('Porcentagem de Sobreviventes e Não Sobreviventes')
+plt.show()
