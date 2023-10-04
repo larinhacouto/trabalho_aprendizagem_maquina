e651c5a74eae2b3e4a5e4f47d0a06598df61f54b
unknown <dherculano93@gmail.com>
2023-10-04 01:18:28 -0300

    adicionando os arquivos do trabalho

---
+++ exer2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import sys
+
+# Questão 2:
+# Apresente no terminal o somatório de homens (male) e de mulheres (female)
+
+# Tenta ler o arquivo CSV
+try:
+    data = pd.read_csv('data/titanic.csv')
+except FileNotFoundError as e:
+    print(f"Erro: O arquivo 'titanic.csv' não foi encontrado.")
+    sys.exit()
+
+homens = (data['sex'] == 'male').sum()
+mulheres = (data['sex'] == 'female').sum()
+
+# Apresenta os resultados no terminal
+print(f"Total de Homens: {homens}")
+print(f"Total de Mulheres: {mulheres}")
---
+++ exer4.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+# Tenta ler o arquivo CSV
+try:
+    data = pd.read_csv('data/titanic.csv')
+except FileNotFoundError as e:
+    print(f"Erro: O arquivo 'titanic.csv' não foi encontrado.")
+    sys.exit()
+
+# Questão 4:
+# Apresente o gráfico de dispersão da Idade pela Tarifa.
+
+age = data['age']
+fare = data['fare']
+
+# Criamos um gráfico de dispersão
+plt.scatter(age, fare, alpha=0.5)
+
+# Definimos rótulos dos eixos e título
+plt.xlabel('Idade')
+plt.ylabel('Tarifa')
+plt.title('Gráfico de Dispersão: Idade pela Tarifa')
+
+# Mostra o gráfico
+plt.grid(True)
+plt.show()
